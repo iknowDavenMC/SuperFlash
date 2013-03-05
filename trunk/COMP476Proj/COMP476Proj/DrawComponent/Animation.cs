024adfff4842c490682dc74239fc2a3291831bae
jeffrey.f.how@gmail.com <jeffrey.f.how@gmail.com@d3740f21-182d-28e3-9b7a-376d15cf4c55>
2013-03-05 21:41:36 +0000

    Initial Entity class Initial DrawComponent (Sprite) classes

---
+++ trunk/COMP476Proj/COMP476Proj/DrawComponent/Animation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace COMP476Proj
+{
+    public class Animation
+    {
+        /*-------------------------------------------------------------------------*/
+        #region Fields
+
+        public string animationId;
+        Texture2D texture;
+        protected int numOfColumns;
+        protected int frameWidth;
+        protected int frameHeight;
+        protected int yPos = 0;
+        #endregion
+
+        /*-------------------------------------------------------------------------*/
+        #region Properties
+
+        public string AnimationId
+        {
+            get { return animationId; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public int FrameWidth
+        {
+            get { return frameWidth; }
+        }
+
+        public int FrameHeight
+        {
+            get { return frameHeight; }
+        }
+
+        public int YPos
+        {
+            get { return yPos; }
+        }
+
+        public int NumOfColumns
+        {
+            get { return numOfColumns; }
+        }
+
+        #endregion
+
+        /*-------------------------------------------------------------------------*/
+        #region Init
+
+        public Animation(string animationId, Texture2D texture)
+        {
+            this.animationId = animationId;
+            this.texture = texture;
+            numOfColumns = 1;
+            frameWidth = texture.Width;
+            frameHeight = texture.Height;
+        }
+
+        public Animation(string animationID, Texture2D texture, int numOfCol, int frameWid, int frameHt, int y)
+        {
+            this.animationId = animationID;
+            this.texture = texture;
+            numOfColumns = numOfCol;
+            frameWidth = frameWid;
+            frameHeight = frameHt;
+            yPos = y;
+        }
+
+        #endregion
+    }
+}
