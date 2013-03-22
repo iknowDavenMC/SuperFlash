95507cf0b2457b361c9dd271f6b0716fceaffa43
jeffrey.f.how <jeffrey.f.how@gmail.com>
2013-03-22 20:39:10 +0000

    Changed Drawing

--- COMP476Proj/COMP476Proj/DrawComponent/StreakerSprite.cs
+++ COMP476Proj/COMP476Proj/DrawComponent/StreakerSprite.cs
@@ -23,7 +23,7 @@ namespace COMP476Proj
         #region Update & Draw
         public override void Update(GameTime gameTime, Entity e)
         {
-            Streaker streaker = (Streaker)e;
+            /*Streaker streaker = (Streaker)e;
             switch (streaker.charState)
             {
                 case StreakerState.STATIC:
@@ -57,8 +57,47 @@ namespace COMP476Proj
                 default:
                     Pause();
                     break;
+            }*/
+            base.Update(gameTime, e);
+        }
+
+
+        public void SetAnimation(StreakerState s, bool flip)
+        {
+            switch (s)
+            {
+                case StreakerState.STATIC:
+                    animation = SpriteDatabase.GetAnimation("streaker_static");
+                    Play();
+                    break;
+                case StreakerState.WALK:
+                    if (flip)
+                    {
+                        spriteEffects = SpriteEffects.FlipHorizontally;
+                    }
+                    else
+                    {
+                        spriteEffects = SpriteEffects.None;
+                    }
+                    animation = SpriteDatabase.GetAnimation("streaker_walk");
+                    Play();
+                    break;
+                case StreakerState.FALL:
+                    animation = SpriteDatabase.GetAnimation("streaker_fall");
+                    Play();
+                    break;
+                case StreakerState.GET_UP:
+                    animation = SpriteDatabase.GetAnimation("streaker_getup");
+                    Play();
+                    break;
+                case StreakerState.DANCE:
+                    animation = SpriteDatabase.GetAnimation("streaker_dance");
+                    Play();
+                    break;
+                default:
+                    Pause();
+                    break;
             }
-            base.Update(gameTime, streaker);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 pos)
