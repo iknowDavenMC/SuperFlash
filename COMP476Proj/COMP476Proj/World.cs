baf63a62bd151a218b2d842790d1c24e5f0a12ca
alex2539 <alex2539@gmail.com>
2013-03-22 18:09:22 +0000

    Added a simple camera.

--- COMP476Proj/COMP476Proj/World.cs
+++ COMP476Proj/COMP476Proj/World.cs
@@ -14,12 +14,14 @@ namespace COMP476Proj
         #region Fields
         public Streaker streaker;
         public List<BoundingBox> mapBoundingBoxes;
+        //public Camera camera;
         #endregion
 
         #region Init
         public World()
         {
             streaker = EntityBuilder.buildStreaker();
+            //camera = new Camera();
         }
         #endregion
 
@@ -27,13 +29,20 @@ namespace COMP476Proj
         public void Update(GameTime gameTime)
         {
             streaker.Update(gameTime);
+            Camera.X = (int)streaker.physics.position.X - Camera.Width/2;
+            Camera.Y = (int)streaker.physics.position.Y - Camera.Height/2;
+            if (Camera.X < 0)
+                Camera.X = 0;
+            if (Camera.Y < 0)
+                Camera.Y = 0;
             //for each enemy --> update
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            Vector2 drawPos = new Vector2(-Camera.X, -Camera.Y);
+            spriteBatch.Draw(SpriteDatabase.GetAnimation("level1").Texture, drawPos, Color.White);
             streaker.Draw(gameTime, spriteBatch);
-            
         }
         #endregion
     }
