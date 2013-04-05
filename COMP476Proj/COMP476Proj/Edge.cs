d069ba3827f620f2a4441575b5e2378e91766da7
jeffrey.f.how <jeffrey.f.how@gmail.com>
2013-04-05 17:07:38 +0000

    Added new animations, node class, edge class

---
+++ COMP476Proj/COMP476Proj/Edge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP476Proj
+{
+    public class Edge
+    {
+        public Node prevNode;
+        public Node currNode;
+        public float costSoFar;
+        public float estimCost;
+        public Edge(Node prevNode, Node currNode, float costSoFar, float estCost)
+        {
+            this.prevNode = prevNode;
+            this.currNode = currNode;
+            this.costSoFar = costSoFar;
+            estimCost = estCost;
+        }
+
+        public bool Equals(Edge e){
+            return prevNode.Equals(e.prevNode) && currNode.Equals(e.currNode) &&
+                costSoFar == e.costSoFar && estimCost == e.estimCost;
+        }
+
+        public override string ToString()
+        {
+            return "["+currNode.NodeID+","+costSoFar+","+prevNode.NodeID+"->"+currNode.NodeID+","+estimCost+"]";
+        }
+    }
+}
---
+++ COMP476Proj/COMP476Proj/Node.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace COMP476Proj
+{
+    public class Node
+    {
+        #region Fields
+        public static int nodeCount = 0;
+        private int nodeID;
+        private Vector2 position;
+        public List<Node> friendList = new List<Node>();
+        #endregion
+
+        #region Properties
+        public Vector2 Position
+        {
+            get { return position; }
+        }
+        public int NodeID
+        {
+            get { return nodeID; }
+        }
+        #endregion
+
+        #region Constructor
+        public Node(int id, float xPos, float yPos)
+        {
+            nodeID = id;
+            position.X = xPos;
+            position.Y = yPos;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return "Node ID: " + nodeID;
+        }
+        #endregion
+    }
+}
