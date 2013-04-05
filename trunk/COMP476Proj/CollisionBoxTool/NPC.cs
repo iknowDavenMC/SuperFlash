0b88d6c0110c104c2ca8e711f63d6ae74b7556f8
alex2539@gmail.com <alex2539@gmail.com@d3740f21-182d-28e3-9b7a-376d15cf4c55>
2013-04-05 19:35:59 +0000

    Updated level editor with player and sprites.

--- trunk/COMP476Proj/CollisionBoxTool/NPC.cs
+++ trunk/COMP476Proj/CollisionBoxTool/NPC.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using StreakerLibrary;
 
 namespace CollisionBoxTool
 {
@@ -15,11 +16,15 @@ namespace CollisionBoxTool
         public int Y { get { return (int)position.Y; } }
         private float radius = 16;
         private Texture2D tex;
-        public enum Type { Civilian, DumbCop, SmartCop }
+        public enum Type { Civilian, DumbCop, SmartCop, Streaker }
         public enum Mode { Wander, Static }
         public Type type;
         public Mode mode;
         public Color color;
+
+        private const int PIXELS_HEAD_TO_TOE = 149;
+        private const int PIXELS_LEFT_TO_CENTER = 88;
+
         public NPC(float x, float y, Type type, Mode mode)
         {
             position = new Vector2(x, y);
@@ -37,20 +42,38 @@ namespace CollisionBoxTool
 
         public void draw(SpriteBatch sb, Texture2D tex, Rectangle camera)
         {
+            Animation anim;
             switch (type)
             {
                 case Type.Civilian:
+                    anim = SpriteDatabase.GetAnimation("student1_static");
                     color = Color.LimeGreen;
                     break;
                 case Type.DumbCop:
+                    anim = SpriteDatabase.GetAnimation("cop_static");
                     color = Color.Crimson;
                     break;
                 case Type.SmartCop:
+                    anim = SpriteDatabase.GetAnimation("cop_static");
+                    color = Color.Orange;
+                    break;
+                case Type.Streaker:
+                    anim = SpriteDatabase.GetAnimation("streaker_static");
+                    color = Color.Goldenrod;
+                    break;
+                default:
+                    anim = SpriteDatabase.GetAnimation("student2_static");
                     color = Color.Orange;
                     break;
             }
-            Vector2 drawPos = new Vector2(position.X - radius - camera.X, position.Y - radius - camera.Y);
-            sb.Draw(tex, drawPos, color);
+            //color.A = 128;
+            Vector2 offset = new Vector2(PIXELS_LEFT_TO_CENTER * 0.4f, PIXELS_HEAD_TO_TOE * 0.4f);
+
+            Vector2 drawPos = new Vector2(position.X - camera.X, position.Y - camera.Y) - offset;
+            Vector2 radOff = new Vector2(-radius, -radius);
+            sb.Draw(tex, drawPos+radOff+offset, color);
+            Rectangle sourceRect = new Rectangle(anim.FrameWidth, anim.YPos, anim.FrameWidth, anim.FrameHeight);
+            sb.Draw(anim.Texture, drawPos, sourceRect, Color.White, 0, Vector2.Zero, 0.4f, SpriteEffects.None, 0);
         }
     }
 }
