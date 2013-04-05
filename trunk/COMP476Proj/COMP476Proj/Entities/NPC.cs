29e637899c5789247f4ddf1d74d1af92bb5053eb
alex2539@gmail.com <alex2539@gmail.com@d3740f21-182d-28e3-9b7a-376d15cf4c55>
2013-04-05 20:19:08 +0000

    Fixed NPC and Pedestrian to use StreakerLibrary.

--- trunk/COMP476Proj/COMP476Proj/Entities/NPC.cs
+++ trunk/COMP476Proj/COMP476Proj/Entities/NPC.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using StreakerLibrary;
 
 namespace COMP476Proj.Entities
 {
--- trunk/COMP476Proj/COMP476Proj/Entities/Pedestrian.cs
+++ trunk/COMP476Proj/COMP476Proj/Entities/Pedestrian.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using StreakerLibrary;
 #endregion
 
 namespace COMP476Proj.Entities
