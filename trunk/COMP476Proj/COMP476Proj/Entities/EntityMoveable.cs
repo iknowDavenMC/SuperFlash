9dedc37d212f906d0c12c34fab50372b7fa44874
jeffrey.f.how@gmail.com <jeffrey.f.how@gmail.com@d3740f21-182d-28e3-9b7a-376d15cf4c55>
2013-04-05 15:12:31 +0000

    Updated Entity class and subclasses

---
+++ trunk/COMP476Proj/COMP476Proj/Entities/EntityMoveable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP476Proj
+{
+    public class EntityMoveable : EntityVisible
+    {
+        #region Fields
+        protected PhysicsComponent2D physics;
+        #endregion
+
+        #region Properties
+        public PhysicsComponent2D ComponentPhysics
+        {
+            get { return physics; }
+        }
+        #endregion
+
+    }
+}
