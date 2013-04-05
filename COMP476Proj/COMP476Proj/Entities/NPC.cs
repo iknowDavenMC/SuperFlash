256ea093309e60cdc5846924e799d0353afdb354
jeffrey.f.how <jeffrey.f.how@gmail.com>
2013-04-05 18:43:34 +0000

    started Pedestrian class

--- COMP476Proj/COMP476Proj/Entities/NPC.cs
+++ COMP476Proj/COMP476Proj/Entities/NPC.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace COMP476Proj.Entities
 {
-    public class NPC : EntityMoveable
+    public abstract class NPC : EntityMoveable
     {
         #region Fields
         protected MovementAIComponent2D movement;
+        protected float detectRadius = 40f;
         #endregion
 
         #region Properties
-        public MovementAIComponent2D ComponentBrain
+        public MovementAIComponent2D ComponentMovement
         {
             get { return movement; }
         }
         #endregion
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
     }
 }
---
+++ COMP476Proj/COMP476Proj/Entities/Pedestrian.cs
@@ -0,0 +1,159 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+#endregion
+
+namespace COMP476Proj.Entities
+{
+    public enum PedestrianState { STATIC, WANDER, FLEE, PATH, FALL, GET_UP };
+    public enum PedestrianBehavior { DEFAULT, AWARE, COLLIDE };
+    public class Pedestrian : NPC
+    {
+        #region Attributes
+
+        /// <summary>
+        /// Direction the character is moving
+        /// </summary>
+        private Vector2 direction;
+        private PedestrianState state;
+        private PedestrianBehavior behavior;
+        private string studentType;
+        #endregion
+
+        #region Constructors
+        public Pedestrian(PhysicsComponent2D phys, MovementAIComponent2D move, DrawComponent draw, PedestrianState pState)
+        {
+            movement = move;
+            physics = phys;
+            this.draw = draw;
+            state = pState;
+            studentType = draw.animation.animationId.Substring(0, 8);
+        }
+
+        public Pedestrian(PhysicsComponent2D phys, MovementAIComponent2D move, DrawComponent draw, PedestrianState pState,
+            float radius)
+        {
+            movement = move;
+            physics = phys;
+            this.draw = draw;
+            state = pState;
+            detectRadius = radius;
+            studentType = draw.animation.animationId.Substring(0, 8);
+        }
+        #endregion
+
+        #region Public Methods
+
+        public void Update(GameTime gameTime, World w)
+        {
+            if (behavior == PedestrianBehavior.DEFAULT)
+            {
+                
+                if (rect.Collides(w.streaker.BoundingRectangle))
+                {
+                    behavior = PedestrianBehavior.COLLIDE;
+                    return;
+                }
+
+                if(Vector2.Distance(w.streaker.Position, pos) > detectRadius){
+                    behavior = PedestrianBehavior.AWARE;
+                    return;
+                }
+
+                switch (state)
+                {
+                    case PedestrianState.STATIC:
+                        physics.SetTargetValues(true, null, null, null);
+                        break;
+                    case PedestrianState.WANDER:
+                        
+                        break;
+                    case PedestrianState.PATH:
+                        //TO DO
+                        break;
+                    default:
+                        state = PedestrianState.WANDER;
+                        break;
+                }
+            }
+            else if (behavior == PedestrianBehavior.AWARE)
+            {
+                if (rect.Collides(w.streaker.BoundingRectangle))
+                {
+                    behavior = PedestrianBehavior.COLLIDE;
+                    return;
+                }
+
+                switch (state)
+                {
+                    case PedestrianState.WANDER:
+                        break;
+                    case PedestrianState.FLEE:
+                        break;
+                    default:
+                        state = PedestrianState.WANDER;
+                        break;
+                }
+            }
+            else
+            {
+                switch (state)
+                {
+                    case PedestrianState.FALL:
+                        if (draw.animComplete)
+                        {
+                            draw.animation = SpriteDatabase.GetAnimation(studentType + "_getup");
+                            state = PedestrianState.GET_UP;
+                            draw.Reset();
+                        }
+                        else
+                        {
+                            draw.animation = SpriteDatabase.GetAnimation(studentType + "_fall");
+                            draw.Play();
+                        }
+                    
+                        break;
+                    case PedestrianState.GET_UP:
+                        if (draw.animComplete)
+                        {
+                            behavior = PedestrianBehavior.AWARE;
+                            state = PedestrianState.WANDER;
+                            draw.animation = SpriteDatabase.GetAnimation(studentType + "_walk");
+                            draw.Reset();
+                        }
+                        else
+                        {
+                            draw.animation = SpriteDatabase.GetAnimation(studentType + "_getup");
+                            draw.Play();
+                        }
+
+                        break;
+                    default:
+                        state = PedestrianState.FALL;
+                        break;
+                }
+            }
+            /*if (charState != StreakerState.FALL && charState != StreakerState.GET_UP)
+            {
+                handleUserInput(gameTime);
+            }
+
+            physics.UpdatePosition(gameTime.ElapsedGameTime.TotalSeconds);
+            physics.UpdateOrientation(gameTime.ElapsedGameTime.TotalSeconds);
+
+            draw.Update(gameTime, this);
+            UpdateStates(draw.animComplete);
+
+            */
+            //Debugger.getInstance().pointsToDraw.Add(physics.position);
+            base.Update(gameTime);
+        }
+
+        #endregion
+
+
+    }
+}
