154dd6548d06fa3cc60b8f67de0ad5f933283c7f
jeffrey.f.how <jeffrey.f.how@gmail.com>
2013-04-05 16:21:12 +0000

    added npc

---
+++ COMP476Proj/COMP476Proj/Entities/NPC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP476Proj.Entities
+{
+    public class NPC : EntityMoveable
+    {
+        #region Fields
+        protected MovementAIComponent2D movement;
+        #endregion
+
+        #region Properties
+        public MovementAIComponent2D ComponentBrain
+        {
+            get { return movement; }
+        }
+        #endregion
+    }
+}
