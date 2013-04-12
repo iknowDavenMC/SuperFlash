3e3c56932970521cbe15848465021b5ce23b93f3
constablebumley <constablebumley@yahoo.ca>
2013-04-12 14:46:37 +0000

    With banner load

--- COMP476Proj/COMP476Proj/Managers/AchievementManager.cs
+++ COMP476Proj/COMP476Proj/Managers/AchievementManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace COMP476Proj
 {
@@ -31,6 +32,11 @@ namespace COMP476Proj
             if (instance == null)
                 instance = new AchievementManager();
             return instance;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            AchievementToast.LoadContent(content);
         }
 
         public bool AddAchievement(Achievement achv)
