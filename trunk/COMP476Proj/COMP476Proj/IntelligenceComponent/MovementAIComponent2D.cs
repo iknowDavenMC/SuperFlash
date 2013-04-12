53d30bd019702367d194535135baec1f6aae63a7
constablebumley@yahoo.ca <constablebumley@yahoo.ca@d3740f21-182d-28e3-9b7a-376d15cf4c55>
2013-04-12 03:37:16 +0000

    Improved wander

--- trunk/COMP476Proj/COMP476Proj/IntelligenceComponent/MovementAIComponent2D.cs
+++ trunk/COMP476Proj/COMP476Proj/IntelligenceComponent/MovementAIComponent2D.cs
@@ -486,14 +486,10 @@ namespace COMP476Proj
             {
                 do
                 {
-                    angle = MathHelper.ToRadians(30 * (float)normallyDistributedRandomNumber());
+                    angle = MathHelper.ToRadians(5 * (float)normallyDistributedRandomNumber());
                     Vector2 previousDirection = physics.Direction;
                     direction.X = (float)Math.Cos(angle) * previousDirection.X - (float)Math.Sin(angle) * previousDirection.Y;
                     direction.Y = (float)Math.Sin(angle) * previousDirection.X + (float)Math.Cos(angle) * previousDirection.Y;
-                    /*
-                    direction.X = (float)Math.Cos(physics.Orientation + angle);
-                    direction.Y = (float)Math.Sin(physics.Orientation + angle);
-                     * */
                     direction.Normalize();
                 }
                 while (direction.Length() == 0);
