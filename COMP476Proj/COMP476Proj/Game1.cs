4cc960b3fe908ceb5985a8b6fc065e5ba43a6480
constablebumley <constablebumley@yahoo.ca>
2013-04-15 21:29:27 +0000

    Got rid of the particle spewers.

--- COMP476Proj/COMP476Proj/Game1.cs
+++ COMP476Proj/COMP476Proj/Game1.cs
@@ -29,9 +29,6 @@ namespace COMP476Proj
         public static float elapsedTime;
         FrameRate frameRate;
 
-        ParticleSpewer ps;
-        ParticleSpewer ps2;
-
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -98,31 +95,6 @@ namespace COMP476Proj
             Texture2D blank = new Texture2D(GraphicsDevice, 1, 1);
             blank.SetData(new[] { Color.White });
             SpriteDatabase.AddAnimation(new Animation("blank", blank, 1, 1, 1, 0, 1));
-
-            // Two example particle spewers. One is stuck to the HUD and always on, 
-            // the other will follow the streaker and only turn on when he dances
-            ps = new ParticleSpewer(
-                150, 555, // Start position
-                1000, 2, // Max particles and emitters
-                MathHelper.ToRadians(247.5f), // Angle range to spew at
-                MathHelper.ToRadians(292.5f), // This will be a narrow cone upward
-                50, 1000, // Lifespan range (up to one second of life)
-                3, 100, // Particle size and speed (3-pixel particles at 100 pixels/s
-                0, 360, 1, 1, 1, 1, // Min and max Hue, Saturation and Value (This uses any colour at full saturation and value)
-                true, 0.5f); // Fade out after half the lifespan
-            ps.Absolute = true; // The camera does not affect this particle spewer
-            ps.Start();
-
-            ps2 = new ParticleSpewer(
-                100, 75, // Start at the streaker's chest
-                1000, 36, // Shoot a lot of particles at once
-                MathHelper.ToRadians(0), // Spew in every direction
-                MathHelper.ToRadians(360),
-                0, 500, // Live for up to half a second
-                2, 200, // 2-pixel particles moving at 200 pixels/s
-                150, 240, 0, 1, 1, 1, // Colours range from cyan to blue, with and saturation and full value
-                true, 0.9f); // Only fade at the end
-
         }
 
         /// <summary>
@@ -148,18 +120,7 @@ namespace COMP476Proj
                 this.Exit();
             //Debugger.getInstance().Clear();
 
-            // Start the spewer while dancing
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
-            {
-                ps2.Start();
-            }
-            else ps2.Stop();
-
             world.Update(gameTime);
-            ps.Update(gameTime);
-            ps2.X = world.streaker.X;
-            ps2.Y = world.streaker.Y - 25;
-            ps2.Update(gameTime);
             Camera.Update(gameTime);
 //#if (DEBUG)
             {
@@ -185,8 +146,6 @@ namespace COMP476Proj
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, null, null, null, null, transform);
             world.Draw(gameTime, spriteBatch);
             HUD.getInstance().Draw(gameTime, spriteBatch);
-            ps2.Draw(gameTime, spriteBatch);
-            ps.Draw(gameTime, spriteBatch);
             //Debugger.getInstance().Draw(spriteBatch);
 
             foreach (Pedestrian ped in world.pedestrians)
