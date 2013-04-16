6edc25a1d940ca2c485143d2eb39dbc2eef28377
alex2539@gmail.com <alex2539@gmail.com@d3740f21-182d-28e3-9b7a-376d15cf4c55>
2013-04-16 20:51:27 +0000

    Added score number popups.

---
+++ trunk/COMP476Proj/COMP476Proj/DrawComponent/ScorePopup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace COMP476Proj
+{
+    class ScorePopup
+    {
+        private Vector2 position;
+        private int value;
+        private int age, lifespan;
+        private const int riseSpeed = 50;
+        private bool done;
+        private SpriteFont font;
+        public bool IsDone { get { return done; } }
+
+        public ScorePopup(float x, float y, int value, int life)
+        {
+            position = new Vector2(x, y);
+            this.value = value;
+            age = 0;
+            lifespan = life;
+            font = FontManager.getInstance().getFont("AchieveTitle");
+            position.X -= font.MeasureString(value.ToString()).X / 2;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float time = gameTime.ElapsedGameTime.Milliseconds;
+            age += (int)time;
+            if (age >= lifespan)
+            {
+                done = true;
+            }
+            else
+            {
+                position.Y -= riseSpeed * time/1000f;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.DrawString(font, value.ToString(), position, Color.Black);
+            spriteBatch.DrawString(font, value.ToString(), position + new Vector2(1,-1), Color.White);
+        }
+    }
+}
