33fcd6735b6ca8d5a21fb533c2891a7d57c8687a
AddisonRodomista <addisonrodomista@gmail.com>
2013-04-16 19:39:03 +0000

    Organized HUD a bit

--- COMP476Proj/COMP476Proj/UI/HUD.cs
+++ COMP476Proj/COMP476Proj/UI/HUD.cs
@@ -20,22 +20,11 @@ namespace COMP476Proj
         #region Attributes
 
         private static volatile HUD instance = null;
-        //Would be a lot easier to move all of these variables into their own classes called HUDElements or smth
-        //Black Bar
-        private Texture2D banner;
-        private Vector2 positionBanner;
-        private Vector2 bannerScale;
 
-        //Health Container
-        private Texture2D healthBar;
-        private Vector2 positionHealthBar;
-        private Vector2 healthBarScale;
-        private const int healthBarLengthMax = 580;
-
-        //Green Meter
-        private Texture2D healthBarContainer;
-        private Vector2 positionHealthBarContainer;
-        private Vector2 healthBarContainerScale;
+        private List<HudComponent> hudComponents;
+        HudComponent bannerComponent;
+        HudComponent healthBarComponent;
+        HudComponent healthBarContainerComponent;
 
         //Health Variables
         private float health;
@@ -88,7 +77,7 @@ namespace COMP476Proj
 
         private ParticleSpewer particleBar;
 
-        public float Height { get { return bannerScale.Y; } }
+        public float Height { get { return bannerComponent.getSize().Y; } }
         #endregion
 
         /*-------------------------------------------------------------------------*/
@@ -96,21 +85,21 @@ namespace COMP476Proj
 
         private HUD()
         {
-            //Set window size 
+            //Set window properties
             windowWidth = Game1.SCREEN_WIDTH;
             windowHeight = Game1.SCREEN_HEIGHT;
 
-            //Set scale of the textures
-            bannerScale = new Vector2(800, 45);
-            healthBarScale = new Vector2(580, 12);
-            healthBarContainerScale = new Vector2(590, 19);
+            hudComponents = new List<HudComponent>();
+            //Components
+            bannerComponent = new HudComponent(new Vector2(windowWidth / 2, windowHeight - 20), new Vector2(800, 45));
+            healthBarComponent = new HudComponent(new Vector2(bannerComponent.getPosition().X-280, bannerComponent.getPosition().Y), new Vector2(580, 12));
+            healthBarComponent.setOriginLeft();
+            healthBarContainerComponent = new HudComponent(new Vector2(bannerComponent.getPosition().X + -284, bannerComponent.getPosition().Y + 10), new Vector2(590, 19));
+            healthBarContainerComponent.setOriginBottomLeft();
 
-            //Set positions of Textures 
-            positionBanner = new Vector2(windowWidth / 2, windowHeight - 20);
-            positionHealthBar = new Vector2(positionBanner.X - 280, positionBanner.Y);
-            positionHealthBarContainer = new Vector2(positionBanner.X + -284, positionBanner.Y + 10);
-            positionScore = new Vector2(positionBanner.X - (bannerScale.X / 2) + 10, positionBanner.Y + 4);
-            positionTime = new Vector2(positionBanner.X + 355, positionBanner.Y - 12);
+            //Score Positions
+            positionScore = new Vector2(bannerComponent.getPosition().X - (bannerComponent.getSize().X / 2) + 10, bannerComponent.getPosition().Y + 4);
+            positionTime = new Vector2(bannerComponent.getPosition().X + 355, bannerComponent.getPosition().Y - 12);
 
             //Initialize current score
             currentScore = 0;
@@ -147,20 +136,27 @@ namespace COMP476Proj
             playGameOverMusic = true;
 
             particleBar = new ParticleSpewer(
-                positionHealthBar.X + health / 100f * healthBarScale.X - 1, positionHealthBar.Y,
+                healthBarComponent.getPosition().X + health / 100f * healthBarComponent.getSize().X - 1, healthBarComponent.getPosition().Y,
                 50, 20, MathHelper.ToRadians(-90), MathHelper.ToRadians(90),
                 50, 200, 2, 200, 90, 90, 0.5f, 1, 1, 1, true, 0.75f);
             particleBar.Absolute = true;
+
+            
         }
 
         public void loadContent(Texture2D banner, Texture2D notorietyBar, Texture2D notorietyMeter, SpriteFont spriteFont, Texture2D fadeToBlack, Texture2D gameOverText)
         {
             this.spriteFont = spriteFont;
-            this.banner = banner;
-            this.healthBar = notorietyBar;
-            this.healthBarContainer = notorietyMeter;
             this.fadeToBlack = fadeToBlack;
             this.gameOverText = gameOverText;
+
+            //Now using HUDComponents
+            this.bannerComponent.LoadContent(banner);
+            this.healthBarComponent.LoadContent(notorietyBar);
+            this.healthBarContainerComponent.LoadContent(notorietyMeter);
+            hudComponents.Add(bannerComponent);
+            hudComponents.Add(healthBarComponent);
+            hudComponents.Add(healthBarContainerComponent);
         }
 
         public static HUD getInstance()
@@ -202,6 +198,9 @@ namespace COMP476Proj
             }
             particleBar.Update(gameTime);
             UpdateScoreSize(gameTime);
+
+            //Update health bar size
+            healthBarComponent.setXScale((int)updateHealthBar(gameTime));
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -211,15 +210,20 @@ namespace COMP476Proj
             offset *= scale;
             offset.X += Camera.X;
             offset.Y += Camera.Y;
-            spriteBatch.Draw(banner, positionBanner * scale + offset, null, Color.White, 0.0f, new Vector2(bannerScale.X / 2, bannerScale.Y / 2), scale, SpriteEffects.None, 0f);
-            spriteBatch.Draw(healthBar, positionHealthBar * scale + offset, new Rectangle(0, 0, (int)updateHealthBar(gameTime), (int)healthBarScale.Y), Color.White, 0.0f, new Vector2(0, healthBarScale.Y / 2), scale, SpriteEffects.None, 0f);
-            spriteBatch.Draw(healthBarContainer, positionHealthBarContainer * scale + offset, null, Color.White, 0.0f, new Vector2(0.0f, healthBarContainerScale.Y), scale, SpriteEffects.None, 0f);
+
+            //Components
+            foreach (HudComponent component in hudComponents)
+            {
+                component.Draw(gameTime, spriteBatch, scale, offset);
+            }
+
             spriteBatch.DrawString(spriteFont, displayedScore.ToString(), positionScore * scale + offset, Color.White, 0f, new Vector2(0, 15), scoreScale, SpriteEffects.None, 0f);
             spriteBatch.DrawString(spriteFont, minutes + ":" + displaySeconds + seconds, positionTime * scale + offset, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
             particleBar.Draw(gameTime, spriteBatch);
             drawGameOver(gameTime, spriteBatch, offset, scale);
+            
         }
-
+        
         public void drawGameOver(GameTime gameTime, SpriteBatch spriteBatch, Vector2 offset, float scale)
         {
             //If the game is over, draw the sprites
@@ -234,7 +238,7 @@ namespace COMP476Proj
                 }
                 //Draw black Alpha
                 animateFadeToBlack(gameTime);
-                spriteBatch.Draw(fadeToBlack, fadeToBlackPosition * scale + offset, Color.White*fadeToBlackAlpha);
+                spriteBatch.Draw(fadeToBlack, new Rectangle((int)(0 + offset.X), (int)(0 + offset.Y), (int)(windowWidth * scale + offset.X), (int)(windowHeight * scale + offset.Y)), Color.Black * fadeToBlackAlpha);
 
                 //Draw Game Over Text
                 interpolate(ref gameOverTextScale, 1.0f, ref gameOverCurrentTime, 0.7f, gameTime);
@@ -293,9 +297,9 @@ namespace COMP476Proj
         {
             particleBar.Stop();
             interpolate(ref health, healthActual, ref timeSoFar, TimeToAnimate, gameTime);
-            particleBar.X = positionHealthBar.X + (health / 100.0f) * healthBarScale.X - 1;
-            
-            return ((health / 100.0f) * healthBarScale.X);
+            particleBar.X = healthBarComponent.getPosition().X + (health / 100.0f) * healthBarComponent.getSize().X - 1;
+
+            return ((health / 100.0f) * healthBarComponent.getSize().X);
         }
 
         //Update the score size 
