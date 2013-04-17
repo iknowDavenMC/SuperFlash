ca517febd6e302049466e283032b41c5d057e1c8
alex2539 <alex2539@gmail.com>
2013-04-17 02:47:20 +0000

    Made camera zoom in on the dance. Fixed score being affected by zoom and also made it inflate from the center instead of the corner.

--- COMP476Proj/COMP476Proj/DrawComponent/Camera.cs
+++ COMP476Proj/COMP476Proj/DrawComponent/Camera.cs
@@ -13,30 +13,34 @@ namespace COMP476Proj
     {
         #region Members
         private static Rectangle bounds = new Rectangle(Game1.SCREEN_WIDTH / 2, Game1.SCREEN_HEIGHT / 2, Game1.SCREEN_WIDTH, Game1.SCREEN_HEIGHT);
-        private static int maxX = Game1.SCREEN_WIDTH;
-        private static int maxY = Game1.SCREEN_HEIGHT;
+        private static float maxX = Game1.SCREEN_WIDTH / 2;
+        private static float maxY = Game1.SCREEN_HEIGHT / 2;
         private static float scale = 1f;
+        private static float targetScale = 1f;
+        private static float timeToZoom = 300;
+        private static float timeZooming = 0;
+        private static float oldScale = 1f;
 
         public static int X { get { return bounds.X; } set { bounds.X = value; } }
         public static int Y { get { return bounds.Y; } set { bounds.Y = value; } }
         public static int Width { get { return bounds.Width; } }
         public static int Height { get { return bounds.Height; } }
-        public static int MaxX
+        public static float MaxX
         {
             get { return maxX; }
             set
             {
-                value -= (int)(Width / 2 / scale); // Keeps X centered without going past the intended X
-                maxX = (value < Width / 2 / scale ? (int)(Width / 2 / scale) : value);
+                value -= (Width / 2 / scale); // Keeps X centered without going past the intended X
+                maxX = (value < Width / 2 / scale ? (Width / 2 / scale) : value);
             }
         }
-        public static int MaxY
+        public static float MaxY
         {
             get { return maxY; }
             set
             {
-                value -= (int)((Height) / 2 / scale - HUD.getInstance().Height); // Keeps Y centered without going past the intended Y
-                maxY = (value < (Height) / 2 / scale - HUD.getInstance().Height ? (int)((Height) / 2 / scale - HUD.getInstance().Height) : value);
+                value -= (Height) / 2 / scale - HUD.getInstance().Height; // Keeps Y centered without going past the intended Y
+                maxY = (value < (Height) / 2 / scale - HUD.getInstance().Height ? ((Height) / 2 / scale - HUD.getInstance().Height) : value);
             }
         }
         public static float Scale
@@ -44,12 +48,12 @@ namespace COMP476Proj
             get { return scale; }
             set
             {
-                maxX += (int)((Width / 2) * scale);
-                maxY += (int)((Height / 2) * scale);
-                maxX -= (int)((Width / 2) / value);
-                maxY -= (int)((Height / 2) / value);
-
-                scale = Math.Abs(value);
+                if (value != targetScale)
+                {
+                    targetScale = Math.Abs(value);
+                    oldScale = scale;
+                    timeZooming = 0;
+                }
             }
         }
 
@@ -62,6 +66,7 @@ namespace COMP476Proj
         #region Public Methods
         public static void Update(GameTime gameTime)
         {
+            //maxY = Map.HEIGHT - Height/2;
             if (Target != null)
             {
                 X = (int)Target.X;
@@ -75,9 +80,22 @@ namespace COMP476Proj
             if (Y < bounds.Height / 2 / scale)
                 Y = (int)(bounds.Height / 2 / scale);
             if (X > maxX)
-                X = maxX;
+                X = (int)maxX;
             if (Y > maxY)
-                Y = maxY;
+                Y = (int)maxY;
+
+            if (Math.Abs(scale - targetScale) > float.Epsilon && timeZooming < timeToZoom)
+            {
+                timeZooming += gameTime.ElapsedGameTime.Milliseconds;
+                maxY = (Map.HEIGHT) - (Height / 2 - HUD.getInstance().Height) / scale;
+                scale = (timeZooming / timeToZoom) * (targetScale - oldScale) + oldScale;
+                //maxY = 948.5f;// Height* scale;
+            }
+            else
+            {
+                timeZooming = 0;
+                scale = targetScale;
+            }
         }
         #endregion
     }
--- COMP476Proj/COMP476Proj/UI/HUD.cs
+++ COMP476Proj/COMP476Proj/UI/HUD.cs
@@ -226,7 +226,8 @@ namespace COMP476Proj
                 component.Draw(gameTime, spriteBatch, scale, offset);
             }
 
-            spriteBatch.DrawString(spriteFont, displayedScore.ToString(), positionScore * scale + offset, Color.White, 0f, new Vector2(0, 15), scoreScale, SpriteEffects.None, 0f);
+            float scoreOffset = spriteFont.MeasureString(displayedScore.ToString()).X / 2;
+            spriteBatch.DrawString(spriteFont, displayedScore.ToString(), (positionScore + new Vector2(scoreOffset, 0)) * scale + offset, Color.White, 0f, new Vector2(scoreOffset, 15), scoreScale * scale, SpriteEffects.None, 0f);
             spriteBatch.DrawString(spriteFont, minutes + ":" + displaySeconds + seconds, positionTime * scale + offset, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
             particleBar.Draw(gameTime, spriteBatch);
             drawGameOver(gameTime, spriteBatch, offset, scale);
