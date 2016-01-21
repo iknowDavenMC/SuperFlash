c0b336b7aa49c720709d9201706430f1782cea1e
David Campbell <constablebumley@yahoo.ca>
2016-01-21 16:15:55 -0500

    Basic camera movement set up
    The camera now follows the player while remaining between specified
    bounds

---
+++ SuperFlash/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+		// TODO: Figure out how to determine these values
+		public Rect m_cameraBounds;
+		public Vector3 m_offset;
+
+		private Camera m_camera;
+		private Transform m_target;
+
+		// Use this for initialization
+		void Start()
+		{
+				m_camera = Camera.main;
+				m_target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+
+		// Update is called once per frame
+		void FixedUpdate()
+		{
+				Vector3 desiredPosition = m_target.position + m_offset;
+
+				desiredPosition.x = Mathf.Clamp(desiredPosition.x, m_cameraBounds.xMin, m_cameraBounds.xMax);
+				desiredPosition.y = Mathf.Clamp(desiredPosition.y, m_cameraBounds.yMin, m_cameraBounds.yMax);
+
+				m_camera.transform.position = desiredPosition;
+		}
+}
